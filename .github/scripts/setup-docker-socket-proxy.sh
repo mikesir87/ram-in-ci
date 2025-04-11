cc5e38a5acd557e48be6f702dab62b858045e87e
Michael Irwin <mikesir87@gmail.com>
2025-04-11 09:21:46 -0700

    Helps to actually save the file

--- .github/scripts/setup-docker-socket-proxy.sh
+++ .github/scripts/setup-docker-socket-proxy.sh
@@ -3,7 +3,7 @@
 set -e
 
 ACCESS_TOKEN=$(curl https://hub.docker.com/v2/auth/token -s -X POST -H "Content-type: application/json" -d "{\"identifier\":\"${DOCKERHUB_USERNAME}\",\"secret\":\"${DOCKERHUB_TOKEN}\"}" | jq .access_token -r)
-curl "https://hub.docker.com/v2/orgs/${DOCKERHUB_ORG}/settings/registry-access-management/policy" -s -H "Authorization: Bearer $ACCESS_TOKEN"
+curl -v "https://hub.docker.com/v2/orgs/${DOCKERHUB_ORG}/settings/registry-access-management/policy" -s -H "Authorization: Bearer $ACCESS_TOKEN"
 ALLOWED_REGISTRIES=$(curl "https://hub.docker.com/v2/orgs/${DOCKERHUB_ORG}/settings/registry-access-management/policy" -s -H "Authorization: Bearer $ACCESS_TOKEN" | jq -c '.allowed | map(.value)')
 
 CONFIG_DATA="{\"mutators\":[{ \"type\": \"addLabels\", \"labels\": { \"protected\": \"true\" }}],\"gates\":[{ \"type\": \"registry\", \"registries\": ${ALLOWED_REGISTRIES} }], \"responseFilters\":[{ \"type\": \"labelFilter\", \"requiredLabels\": { \"protected\": \"true\" }}]}"
